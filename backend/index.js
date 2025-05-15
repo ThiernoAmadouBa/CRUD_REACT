288a202861ad8882db9d145067c9cd830f3f9aa7
ThiernoAmadouBa <bathierno1910@gmail.com>
2025-05-15 05:35:58 +0000

    save

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, )
   .then(() => app.listen(process.env.PORT, () => console.log("🚀 Backend running on port", process.env.PORT)))
-  .catch(err => console.error(err));
+  .catch(err => {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  });
